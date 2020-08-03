3129b180231251c8ec247c6c15f0e31135ca110d
jammoo <dinoman0624@hotmail.com>
2020-08-03 17:14:20 -0400

    Added headerfooter module

---
+++ src/app/modules/headerfooter/footer/footer.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  sname = "James Bhagoutie";
+  sid = 991554085;
+  date = new Date();
+  year = this.date.getFullYear();
+  months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+  month = this.months[this.date.getMonth()];
+  day = this.date.getDate();
+  
+  ngOnInit(): void {
+  }
+
+}
